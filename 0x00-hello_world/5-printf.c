2f4a29b53698f22e6467c407bb528026f3cb43f0
Sibusisiwe Myeki <sibumyeki@gmail.com>
2022-09-08 16:45:24 -0700

    Task 4. Hello, puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0
+
+ * main - entry point
+
+ * Return: 0
+
  */
+
 int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
-}
+

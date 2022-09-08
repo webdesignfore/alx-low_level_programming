1c2b5ada42fcbe76bb97232212894589521e3cb4
Sibusisiwe Myeki <sibumyeki@gmail.com>
2022-09-08 16:15:57 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#inlude <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using main function
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

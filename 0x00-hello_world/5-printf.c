119eb33e4384a4baff7c046a0c4bd3111e96718d
Sibusisiwe Myeki <sibumyeki@gmail.com>
2022-09-08 18:25:22 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,10 @@
-#include <stdio.h>
-
+ #include <stdio.h>
 /**
-
- * main - Print the string in the printf function
-
-
-
- * Return: Always 0
-
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
-
 {
-
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }

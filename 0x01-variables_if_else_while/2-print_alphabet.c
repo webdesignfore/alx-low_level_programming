a11b8f473b9b8fa69da0de25f13692b37f331aed
Sibusisiwe Myeki <sibumyeki@gmail.com?>
2022-09-12 01:44:50 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,24 +6,14 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	char lower_case;
-	char upper_case;
+	char letter;
 
-	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-	{
-		putchar(lower_case);
-	}
-
-	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-	{	
-	putchar(upper_case);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
 	putchar('\n');
 
 	return (0);
-
 }
